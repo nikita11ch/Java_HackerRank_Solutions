6105f6056830f17e35850972944930905a4a1b4f
Nikita Chaurasia <115834443+nikita11ch@users.noreply.github.com>
2024-03-25 16:58:25 +0530

    stdin and stdout in java (II)

---
+++ Solution.java
@@ -0,0 +1,32 @@
+/*
+ * 
+Sample Input
+42
+3.1415
+Welcome to HackerRank's Java tutorials!
+
+Sample Output
+String: Welcome to HackerRank's Java tutorials!
+Double: 3.1415
+Int: 42
+*/
+
+
+package Java_HackerRank_Solutions;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d=scan.nextDouble();
+        String s=scan.nextLine();
+        s=scan.nextLine();
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+        scan.close();
+    }
+}
